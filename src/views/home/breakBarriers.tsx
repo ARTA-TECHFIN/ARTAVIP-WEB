1dba55817f4f5507f721789cf5a958efb32c6822
CM Ngan <plusonecm@gmail.com>
2022-10-29 17:47:19 +0800

    fix typo and formatting

--- src/views/home/breakBarriers.tsx
+++ src/views/home/breakBarriers.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState, useRef } from 'react'
 import { gsap } from 'gsap'
 
-export const BreakBarriers = ({currentSectionId}: any) => {
+export const BreakBarriers = ({ currentSectionId }: any) => {
   const sec1TtlRef = useRef(null)
   const sec1VideoRef = useRef(null)
   const sec1Container = useRef(null)
-  const [windowDimension, setWindowDemension] = useState<{x: number, y: number}>({x:0,y:0})
+  const [windowDimension, setWindowDimension] = useState<{ x: number; y: number }>({ x: 0, y: 0 })
 
   var request: any = null
   var mouse = { x: 0, y: 0 }
@@ -14,14 +14,14 @@ export const BreakBarriers = ({currentSectionId}: any) => {
   useEffect(() => {
     cx = window?.innerWidth / 50
     cy = window?.innerHeight / 50
-    setWindowDemension({x:cx, y:cy})
+    setWindowDimension({ x: cx, y: cy })
 
     gsap.fromTo(
       sec1TtlRef.current,
       { opacity: 0 },
       {
         opacity: 1,
-        x: "0.5em",
+        x: '0.5em',
         duration: 2,
         ease: 'power0',
         delay: 1,
@@ -62,7 +62,7 @@ export const BreakBarriers = ({currentSectionId}: any) => {
   }
 
   function updateSec1TextPos() {
-    if (cx*2 > 768) {
+    if (cx * 2 > 768) {
       var dx = mouse.x - windowDimension.x
       var dy = mouse.y - windowDimension.y
 
@@ -94,7 +94,7 @@ export const BreakBarriers = ({currentSectionId}: any) => {
     <section
       ref={sec1Container}
       id="break-barriers"
-      className=" relative flex w-screen h-screen overflow-hidden flex-col bg-arta-russet-100 will-change-transform z-3 tigger-01"
+      className=" tigger-01 relative z-3 flex h-screen w-screen flex-col overflow-hidden bg-arta-russet-100 will-change-transform"
       onMouseMove={(ev) => section1HandleMouseMove(ev)}
     >
       <video
@@ -111,20 +111,19 @@ export const BreakBarriers = ({currentSectionId}: any) => {
         Your browser does not support the video tag.
       </video>
       {/* top-[20%] left-0   sm:top-[40%] lg:left-[10%] xl:left-[20%] xl:mx-auto */}
-      <div className=" flex max-w-main-contain items-start justify-center px-6  xl:mx-auto movable-elements-wrapper">
+      <div className=" movable-elements-wrapper flex max-w-main-contain items-start justify-center  px-6 xl:mx-auto">
         <div className="absolute bottom-[7em] left-[0] lg:bottom-[6em] lg:left-[5em]">
           <h1
             id="animation"
             ref={sec1TtlRef}
-            className="z-[200] font-Verah text-[10em] lg:text-[5em] leading-[1.02em] text-white movable text-left will-change-transform"
+            className="movable z-[200] text-left font-Verah text-[10em] leading-[1.02em] text-white will-change-transform lg:text-[5em]"
           >
             BREAK
             <br />
             BARRIERS
             <span className="flex space-x-2">
               <hr className=" mt-[20px] h-[4px] w-[40px] sm:mt-[0.45em] sm:w-[80px]"></hr>
-              <span className="pr-3 text-[30px] sm:text-[0.6em]">for </span>{' '}
-              GREATNESS
+              <span className="pr-3 text-[30px] sm:text-[0.6em]">for </span> GREATNESS
             </span>
           </h1>
         </div>

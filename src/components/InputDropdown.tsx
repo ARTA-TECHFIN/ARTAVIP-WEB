ad3dad81f04a44aca08f9a16da40ab544f3ea622
CM Ngan <plusonecm@gmail.com>
2022-11-15 23:52:43 +0800

    feat(contact us): add desktop and success ui

--- src/components/InputDropdown.tsx
+++ src/components/InputDropdown.tsx
@@ -4,10 +4,13 @@ type propsT = ComponentProps<'select'> & {
   options: Array<{ label: string; value: string }>
 }
 
-const InputDropdown = ({ options, ...rest }: propsT) => {
+const InputDropdown = ({ options, className, ...rest }: propsT) => {
   return (
     <select
-      className="w-full border-b border-solid border-arta-indigo-100 bg-transparent py-3 text-base focus:outline-none"
+      className={
+        'w-full border-b border-solid border-arta-indigo-100 bg-transparent py-3 text-base focus:outline-none' +
+        (className ? ` ${className}` : '')
+      }
       {...rest}
     >
       {options.map((option) => (
--- src/components/PageContact/EnquiryForm.tsx
+++ src/components/PageContact/EnquiryForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { ButtonAnimated } from '../ButtonAnimated'
 import { InputDropdown } from '../InputDropdown'
 import { InputField } from '../InputField'
@@ -6,7 +7,7 @@ import { InputTextArea } from '../InputTextArea'
 import { textClass } from '../Text'
 
 const topicOptions = [
-  { value: 'General', label: 'General' },
+  { value: '', label: 'Please select' },
   { value: 'Media', label: 'Media' },
   { value: 'Other', label: 'Other' },
 ]
@@ -22,44 +23,81 @@ const t = {
   question_message: 'Brief Introduction',
   question_message_hints: 'Max. 500 characters',
   question_submit: 'Submit',
+
+  success_title: 'Thank you!',
+  success_message: '(Dummy text) Thank you for your enquiry, we will contact you very soon.',
 }
 
 const EnquiryForm = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
+  const [topic, setTopic] = useState('')
+
+  const isExpanded = topic !== ''
+
   return (
     <div className="arta-container bg-arta-sand-200 py-12">
       <h2 className={`${textClass.h2_style2} mb-6`}>{t.title}</h2>
-      <form className="grid gap-10 bg-arta-snow-100 p-6 shadow-2xl">
-        <InputField label={t.question_topic}>
-          <InputDropdown options={topicOptions} name="topic" />
-        </InputField>
-        <InputField label={t.question_name}>
-          <InputText name="name" />
-        </InputField>
-        <InputField label={t.question_company}>
-          <InputText name="company" />
-        </InputField>
-        <InputField label={t.question_jobTitle}>
-          <InputText name="jobTitle" />
-        </InputField>
-        <InputField label={t.question_email}>
-          <InputText name="email" />
-        </InputField>
-        <InputField label={t.question_phone}>
-          <InputText name="phone" />
-        </InputField>
-        <InputField label={t.question_message}>
-          <InputTextArea name="message" />
-          <span className={`${textClass.caption} mt-2 text-arta-sand-100`}>
-            {t.question_message_hints}
-          </span>
-        </InputField>
-        <ButtonAnimated
-          extraProps={{ type: 'submit' }}
-          className="w-full border-arta-sand-100 text-arta-sand-100 "
+      {isSubmitted ? (
+        <div className="min-h-[220px] w-full max-w-[820px] flex-col gap-10 bg-arta-snow-100 p-6 shadow-2xl">
+          <h3 className={`${textClass.h6} mb-4`}>{t.success_title}</h3>
+          <p className={`${textClass.body_regular} mb-6`}>{t.success_message}</p>
+        </div>
+      ) : (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            setIsSubmitted(true)
+          }}
+          className={
+            'flex w-full max-w-[820px] flex-col gap-10 bg-arta-snow-100 p-6 shadow-2xl transition-all ease-in-out md:grid md:grid-cols-2' +
+            (isExpanded ? ' md:max-h-[800px]' : ' md:max-h-[200px]')
+          }
         >
-          {t.question_submit}
-        </ButtonAnimated>
-      </form>
+          <InputField label={t.question_topic}>
+            <InputDropdown
+              options={topicOptions}
+              value={topic}
+              onChange={(e) => setTopic(e.target.value)}
+            />
+          </InputField>
+          {isExpanded && (
+            <>
+              <div />
+              <InputField label={t.question_name}>
+                <InputText name="name" />
+              </InputField>
+              <div />
+              <InputField label={t.question_company}>
+                <InputText name="company" />
+              </InputField>
+              <InputField label={t.question_jobTitle}>
+                <InputText name="jobTitle" />
+              </InputField>
+              <InputField label={t.question_email}>
+                <InputText name="email" />
+              </InputField>
+              <InputField label={t.question_phone}>
+                <InputText name="phone" />
+              </InputField>
+              <InputField label={t.question_message} className="col-span-2">
+                <InputTextArea name="message" maxLength={500} />
+                <span className={`${textClass.caption} mt-2 text-arta-sand-100`}>
+                  {t.question_message_hints}
+                </span>
+              </InputField>
+              <div className="col-span-2">
+                <ButtonAnimated
+                  extraProps={{ type: 'submit' }}
+                  className="w-full border-arta-sand-100 text-arta-sand-100 md:w-[120px]"
+                >
+                  {t.question_submit}
+                </ButtonAnimated>
+              </div>
+            </>
+          )}
+        </form>
+      )}
     </div>
   )
 }

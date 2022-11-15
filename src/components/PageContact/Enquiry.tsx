304b97a8e84c1fdf0988624342fb2467ed5a17bb
CM Ngan <plusonecm@gmail.com>
2022-11-15 22:24:19 +0800

    fix(contactUs): formatting

--- src/components/PageContact/Enquiry.tsx
+++ src/components/PageContact/Enquiry.tsx
@@ -1,99 +1,118 @@
-import { useEffect,useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
-import parse from 'html-react-parser';
+import parse from 'html-react-parser'
 import { textClass } from 'src/components/Text'
 
-import {Circle} from '../Svg/Icon'
+import { Circle } from '../Svg/Icon'
 import { Hr } from 'src/components/Hr'
-import {IconFacebook, IconWeChat, IconTwitter, IconLinkedIn} from '../Svg/Icon'
+import { IconFacebook, IconWeChat, IconTwitter, IconLinkedIn } from '../Svg/Icon'
 
+const Enquiry = () => {
+  const circle = useRef(null)
 
-const Enquiry = ()=> {
+  useEffect(() => {
+    gsap.set(circle.current, {
+      x: -200,
+      autoAlpha: 0,
+    })
 
-    const circle = useRef(null)
+    const spinTl = gsap.timeline({
+      repeat: -1,
+    })
 
-    useEffect(()=> {
-        gsap.set(circle.current, {
-            x: -200,
-            autoAlpha: 0
-          })
-      
-          const spinTl= gsap.timeline({
-            repeat: -1,
-          })
-      
-          spinTl.pause()
-      
-          const tl = gsap.timeline({
-              scrollTrigger: {
-                  trigger: circle.current,
-                  start: 'top center+=200',
-                  end: 'top center+=200',
-                  toggleActions: "play play reverse  play",
-                  onEnter: () => spinTl.play()
-              }
-          })
-          
-          tl.to(circle.current, {
-            x: 0,
-            autoAlpha: 1
-          })
-      
-          spinTl.to(circle.current, {
-            duration: 3,
-            rotation: 360
-          })
-    }, [])
-    return(
-        <div className='bg-arta-eggshell-100'>
-             <div className="arta-container mx-auto relative grid grid-cols-12 md:py-[150px] py-12">
-                <div className={`md:col-span-4 col-span-full flex items-center relative` }>
-                    <h2 className={`relative z-10 ${textClass.h2_style2} text-arta-sand-100`}>{`Enquiry`}</h2>
-                    <div className="absolute text-col-circle will-change-transform z-0 -left-[250px]" ref={circle}>
-                        <Circle className='xl:scale-100 lg:scale-75 md:scale-[0.70] md:block hidden'/>
-                    </div>
-                </div>
-                <div className="md:col-span-8 col-span-full md:mt-0 mt-6">
-                    <Hr />
-                    <h4 className={`${textClass.h6} mt-8 text-arta-sand-100`}>{`Headquarters`}</h4>
-                    <div className={`${textClass.body_regular_verah} lg:max-w-[80%] mt-4 mb-8 text-arta-sand-100`}>{parse(`Units 1-2, Level 9, <br>
-                    K11 ATELIER King’s Road,<br>
-                    728 King’s Road, Quarry Bay, Hong Kong`)}</div>
-                    <Hr />
+    spinTl.pause()
 
-                    <h4 className={`${textClass.h6} mt-8 text-arta-sand-100`}>{`Enquiry`}</h4>
-                    <div className='grid grid-cols-12 sm:gap-y-8 gap-y-5 mt-4 mb-8'>
-                        <div className='sm:col-span-6 col-span-full'>
-                            <div className='text-base text-black'>
-                                <div className="font-bold">Media</div>
-                                <a href="mailto:marketing@artatechfin.com" className='underline'>marketing@artatechfin.com</a>
-                            </div>
-                        </div>
-                        <div className='sm:col-span-6 col-span-full'>
-                            <div className='text-base text-black'>
-                                <div className="font-bold">Investor</div>
-                                <a href="mailto:ir@artatechfin.com" className='underline'>ir@artatechfin.com</a>
-                            </div>
-                        </div>
-                        <div className='sm:col-span-6 col-span-full'>
-                            <div className='text-base text-black'>
-                                <div className="font-bold">Recruitment</div>
-                                <a href="mailto:recruit@artatechfin.com" className='underline'>recruit@artatechfin.com</a>
-                            </div>
-                        </div>
-                    </div>
-                    <Hr />
-                    <h4 className={`${textClass.h6} mt-8 text-arta-sand-100`}>{`Follow Us`}</h4>
-                    <div className="flex mt-4">
-                        <a href="#" target='_blank' className="mr-6"><IconFacebook className='w-8 h-8' fill='#593725'/></a>
-                        <a href="#" target='_blank' className="mr-6"><IconTwitter className='w-8 h-8' fill='#593725'/></a>
-                        <a href="#" target='_blank' className="mr-6"><IconLinkedIn className='w-8 h-8' fill='#593725'/></a>
-                        <a href="#" target='_blank' className="mr-6"><IconWeChat className='w-8 h-8' fill='#593725'/></a>
-                    </div>
-                </div>
-            </div>
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: circle.current,
+        start: 'top center+=200',
+        end: 'top center+=200',
+        toggleActions: 'play play reverse  play',
+        onEnter: () => spinTl.play(),
+      },
+    })
+
+    tl.to(circle.current, {
+      x: 0,
+      autoAlpha: 1,
+    })
+
+    spinTl.to(circle.current, {
+      duration: 3,
+      rotation: 360,
+    })
+  }, [])
+  return (
+    <div className="bg-arta-eggshell-100">
+      <div className="arta-container relative mx-auto grid grid-cols-12 py-12 md:py-[150px]">
+        <div className={`relative col-span-full flex items-center md:col-span-4`}>
+          <h2 className={`relative z-10 ${textClass.h2_style2} text-arta-sand-100`}>{`Enquiry`}</h2>
+          <div
+            className="text-col-circle absolute -left-[250px] z-0 will-change-transform"
+            ref={circle}
+          >
+            <Circle className="hidden md:block md:scale-[0.70] lg:scale-75 xl:scale-100" />
+          </div>
         </div>
-    )
+        <div className="col-span-full mt-6 md:col-span-8 md:mt-0">
+          <Hr />
+          <h4 className={`${textClass.h6} mt-8 text-arta-sand-100`}>{`Headquarters`}</h4>
+          <div
+            className={`${textClass.body_regular_verah} mt-4 mb-8 text-arta-sand-100 lg:max-w-[80%]`}
+          >
+            {parse(`Units 1-2, Level 9, <br>
+                    K11 ATELIER King’s Road,<br>
+                    728 King’s Road, Quarry Bay, Hong Kong`)}
+          </div>
+          <Hr />
+
+          <h4 className={`${textClass.h6} mt-8 text-arta-sand-100`}>{`Enquiry`}</h4>
+          <div className="mt-4 mb-8 grid grid-cols-12 gap-y-5 sm:gap-y-8">
+            <div className="col-span-full sm:col-span-6">
+              <div className="text-base text-black">
+                <div className="font-bold">Media</div>
+                <a href="mailto:marketing@artatechfin.com" className="underline">
+                  marketing@artatechfin.com
+                </a>
+              </div>
+            </div>
+            <div className="col-span-full sm:col-span-6">
+              <div className="text-base text-black">
+                <div className="font-bold">Investor</div>
+                <a href="mailto:ir@artatechfin.com" className="underline">
+                  ir@artatechfin.com
+                </a>
+              </div>
+            </div>
+            <div className="col-span-full sm:col-span-6">
+              <div className="text-base text-black">
+                <div className="font-bold">Recruitment</div>
+                <a href="mailto:recruit@artatechfin.com" className="underline">
+                  recruit@artatechfin.com
+                </a>
+              </div>
+            </div>
+          </div>
+          <Hr />
+          <h4 className={`${textClass.h6} mt-8 text-arta-sand-100`}>{`Follow Us`}</h4>
+          <div className="mt-4 flex">
+            <a href="#" target="_blank" className="mr-6">
+              <IconFacebook className="h-8 w-8" fill="#593725" />
+            </a>
+            <a href="#" target="_blank" className="mr-6">
+              <IconTwitter className="h-8 w-8" fill="#593725" />
+            </a>
+            <a href="#" target="_blank" className="mr-6">
+              <IconLinkedIn className="h-8 w-8" fill="#593725" />
+            </a>
+            <a href="#" target="_blank" className="mr-6">
+              <IconWeChat className="h-8 w-8" fill="#593725" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Enquiry
--- src/components/PageContact/PageContact.tsx
+++ src/components/PageContact/PageContact.tsx
@@ -5,8 +5,7 @@ import Footer from 'src/components/Footer'
 import { Seo } from 'src/components/Seo'
 
 import { HeroBanner } from '../HeroBanner'
-import Enquiry  from './Enquiry'
-
+import Enquiry from './Enquiry'
 
 const cms = {
   heroBanner: {
@@ -14,10 +13,8 @@ const cms = {
     description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
     image: '/images/contact-us/banner.png',
     mobileImage: '/images/contact-us/mobile-banner.png',
-  }
-    
+  },
 }
-
 
 const PageContactPage: NextPage = () => {
   return (
@@ -31,8 +28,8 @@ const PageContactPage: NextPage = () => {
           image={cms.heroBanner.image}
           mobileImage={cms.heroBanner.mobileImage}
         />
-         <div className='overflow-hidden bg-arta-eggshell-100'>
-            <Enquiry/>
+        <div className="overflow-hidden bg-arta-eggshell-100">
+          <Enquiry />
         </div>
       </main>
       <Footer textColor="brown" />

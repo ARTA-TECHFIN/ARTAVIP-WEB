7611fa102f4e73184e73e6e54d91a789a59e50e3
Jason Ma <horsehcj@gmail.com>
2022-11-17 07:50:31 +0800

    Add video to about page

--- src/components/PageAbout/SectionMission.tsx
+++ src/components/PageAbout/SectionMission.tsx
@@ -1,9 +1,25 @@
+import React, { useRef, useState } from 'react'
 import Image from 'next/image'
 import mission_bg from './images/mission_bg.png'
 import { textClass } from 'src/components/Text'
 import { t } from './PageAbout'
 
 export const SectionMission = () => {
+  const videoRef = useRef<any>(null)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const toggleVideo = () => {
+    console.log("toggle")
+    if (videoRef.current && !isPlaying) {
+      videoRef.current.play();
+      setIsPlaying(true)
+    } else {
+      console.log("pause")
+      videoRef.current.pause();
+      setIsPlaying(false)
+    }
+  }
+
   return (
     <div className="group/bg relative overflow-hidden" id="mission">
       <div className="easeInOutSine absolute h-full w-full scale-105 overflow-hidden duration-300 group-hover/bg:scale-100">
@@ -16,9 +32,22 @@ export const SectionMission = () => {
         <span className={`${textClass.h2_style2} -translate-x-24 text-arta-snow-100`}>
           {t.mission.subtitle1}
         </span>
-        <span className={`${textClass.h2_style2} translate-x-24 text-arta-snow-100`}>
+        <span className={`${textClass.h2_style2} translate-x-24 text-arta-snow-100 mb-12`}>
           {t.mission.subtitle2}
         </span>
+
+        <video
+          data-keepplaying
+          playsInline
+          ref={videoRef}
+          preload="true"
+          crossOrigin="anonymous"
+          className="will-change-transform max-w-[800px] w-[90%]"
+          onClick={() => toggleVideo()}
+        >
+          <source src="/videos/1017-arta-video-brand-r12_comp.mp4" typeof="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </div>
   )

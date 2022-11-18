8052d0a0a73bb120ddb9841fc2db23f174393a74
CM Ngan <plusonecm@gmail.com>
2022-11-18 12:23:29 +0800

    fix formatting and logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -100,9 +100,8 @@ const pageInfoList: pageInfoItemT[] = [
   },
 ]
 
-  
-  const Header: React.FC<{ textColor?: 'white' | 'brown',  fontSize?: string}> = (props) => {
-  const { textColor = 'white', fontSize = '16px'  } = props
+const Header: React.FC<{ textColor?: 'white' | 'brown'; fontSize?: string }> = (props) => {
+  const { textColor = 'white', fontSize = '16px' } = props
   const textColorClass = textColor === 'white' ? 'text-arta-snow-100' : 'text-arta-russet-100'
   const bgColorClass = textColor === 'white' ? 'bg-arta-russet-100/90' : 'bg-arta-snow-100/95'
   const borderColorClass = textColor === 'white' ? 'border-arta-snow-100' : 'border-arta-russet-100'
@@ -117,29 +116,36 @@ const pageInfoList: pageInfoItemT[] = [
   const setActiveTabIndex = (index: number) => {
     _setActiveTabIndex(index)
 
-    if(selectedTab) return
-      gsap.fromTo('#fadeIn', { opacity: 0 }, { opacity: 1, duration: 0.6, delay: 0.3})
+    if (selectedTab) return
+    gsap.fromTo('#fadeIn', { opacity: 0 }, { opacity: 1, duration: 0.6, delay: 0.3 })
   }
   const selectedTab = activeTabIndex === DEFAULT_TAB_INDEX ? null : pageInfoList[activeTabIndex]
 
-  const changeBackground = () => {
-    console.log(window.scrollY)
-    if (window.scrollY >= 66) {
-      setNavbarBg(true)
-    } else {
-      setNavbarBg(false)
-    }
-  }
-
   useEffect(() => {
+    const changeBackground = () => {
+      // home page no bg is needed. It is working on home page because scrolling is not in home page, its window.scrollY is always 0
+      // 66 is just a number that feels good, better than 0
+      setNavbarBg(window.scrollY >= 66)
+    }
+
     changeBackground()
-    window.addEventListener("scroll", changeBackground)
-  })
+    window.addEventListener('scroll', changeBackground)
+    return () => {
+      window.removeEventListener('scroll', changeBackground)
+    }
+  }, [])
+
+  console.log('navbarBg', navbarBg)
 
   return (
     <div className="fixed z-50 w-full">
       <div onMouseLeave={() => setActiveTabIndex(DEFAULT_TAB_INDEX)}>
-        <header className={cn('w-full px-[4em] pt-[6em] transition duration-300 lg:pt-[2.6em] lg:pb-[2em] xl:mx-auto', navbarBg && 'lg:bg-white lg:bg-opacity-75')}>
+        <header
+          className={cn(
+            'w-full px-[4em] pt-[6em] transition duration-300 lg:pt-[2.6em] lg:pb-[2em] xl:mx-auto',
+            navbarBg && 'lg:bg-white lg:bg-opacity-75'
+          )}
+        >
           <div className="flex w-full justify-between lg:space-x-6">
             <div className="hidden items-center justify-center space-x-8 lg:flex">
               {pageInfoList.map((page, index) => {
@@ -149,48 +155,59 @@ const pageInfoList: pageInfoItemT[] = [
                   <div
                     key={index}
                     className={`group z-[4] flex cursor-pointer items-center justify-center opacity-70 transition hover:opacity-100 ${textColorClass}`}
-                    onMouseEnter={() => page.pages.length > 0 ? setActiveTabIndex(index) : null}
+                    onMouseEnter={() => (page.pages.length > 0 ? setActiveTabIndex(index) : null)}
                   >
-                    {
-                      page.pages.length === 0 && (
-                        <Link className="flex items-start" href={page.href}>
-                          <span style={{fontSize: `${fontSize}`}}>{page.title}</span>
-                        </Link>
-                      )
-                    }
-                    
-                    {
-                      page.pages.length > 0 && (
-                        <>
-                          <span
-                            style={{fontSize: `${fontSize}`}}
-                            className={
-                              `z-[3] leading-[24px] decoration-arta-sunray-100 underline-offset-[20px] transition group-hover:underline` +
-                              (selected ? ' underline' : '')
-                            }
-                          >
-                            {page.pageName}
-                          </span>
-                          <ChevronIcon className="z-[3] ml-1 h-4 w-4 transition group-hover:text-arta-sunray-100" />
-                        </>
-                      )
-                    }
+                    {page.pages.length === 0 && (
+                      <Link className="flex items-start" href={page.href}>
+                        <span style={{ fontSize: `${fontSize}` }}>{page.title}</span>
+                      </Link>
+                    )}
+
+                    {page.pages.length > 0 && (
+                      <>
+                        <span
+                          style={{ fontSize: `${fontSize}` }}
+                          className={
+                            `z-[3] leading-[24px] decoration-arta-sunray-100 underline-offset-[20px] transition group-hover:underline` +
+                            (selected ? ' underline' : '')
+                          }
+                        >
+                          {page.pageName}
+                        </span>
+                        <ChevronIcon className="z-[3] ml-1 h-4 w-4 transition group-hover:text-arta-sunray-100" />
+                      </>
+                    )}
                   </div>
                 )
               })}
             </div>
             <div
-              className="flex flex-col justify-between w-[26px] h-[22px] cursor-pointer lg:hidden"
+              className="flex h-[22px] w-[26px] cursor-pointer flex-col justify-between lg:hidden"
               onClick={() => setShowMenu(!showMenu)}
             >
-              <span className={cn('w-full h-[2px]', textColor == 'white' ? 'bg-[#F4F1E1]' : 'bg-[#593725]')}></span>
-              <span className={cn('w-full h-[2px]', textColor == 'white' ? 'bg-[#F4F1E1]' : 'bg-[#593725]')}></span>
-              <span className={cn('w-full h-[2px]', textColor == 'white' ? 'bg-[#F4F1E1]' : 'bg-[#593725]')}></span>
+              <span
+                className={cn(
+                  'h-[2px] w-full',
+                  textColor == 'white' ? 'bg-[#F4F1E1]' : 'bg-[#593725]'
+                )}
+              ></span>
+              <span
+                className={cn(
+                  'h-[2px] w-full',
+                  textColor == 'white' ? 'bg-[#F4F1E1]' : 'bg-[#593725]'
+                )}
+              ></span>
+              <span
+                className={cn(
+                  'h-[2px] w-full',
+                  textColor == 'white' ? 'bg-[#F4F1E1]' : 'bg-[#593725]'
+                )}
+              ></span>
             </div>
             <div className="z-[1] text-right">
               <Link href="/">
-                <div className="relative cursor-pointer opacity-100 transition hover:opacity-100 md:w-auto md:h-auto w-[78.67px] h-[32px]">
-                  <ArtaLogo className={`${textColorClass} md:w-auto w-full md:h-auto h-full`} />
+                <div className="relative h-[32px] w-[78.67px] cursor-pointer opacity-100 transition hover:opacity-100 md:h-auto md:w-auto">
+                  <ArtaLogo className={`${textColorClass} h-full w-full md:h-auto md:w-auto`} />
                 </div>
               </Link>
             </div>
@@ -209,24 +226,22 @@ const pageInfoList: pageInfoItemT[] = [
                     <h6 className="font-Verah text-2xl">{selectedTab.title}</h6>
                     <p className="font-Neue text-[14px] leading-[20px]">{selectedTab.paragraph}</p>
 
-                    {
-                      selectedTab.buttonText.length > 0 && (
-                        <ButtonAnimated
-                          href={selectedTab.href}
-                          as="a"
-                          className={`w-[260px] p-4 px-6 py-3 font-medium shadow-md ${textColorClass} ${borderColorClass}`}
-                          borderWidth={2}
-                        >
-                          {selectedTab.buttonText}
-                        </ButtonAnimated>
-                      )
-                    }
+                    {selectedTab.buttonText.length > 0 && (
+                      <ButtonAnimated
+                        href={selectedTab.href}
+                        as="a"
+                        className={`w-[260px] p-4 px-6 py-3 font-medium shadow-md ${textColorClass} ${borderColorClass}`}
+                        borderWidth={2}
+                      >
+                        {selectedTab.buttonText}
+                      </ButtonAnimated>
+                    )}
                   </div>
                   <div className="flex flex-col">
                     {selectedTab.pages.map((item, index) => (
                       <li
                         key={index}
-                        style={{fontSize: `${fontSize}`}}
+                        style={{ fontSize: `${fontSize}` }}
                         className={`group relative -translate-x-4 cursor-pointer list-none py-1 px-2 leading-[24px] opacity-70 duration-300 ease-out hover:translate-x-0 hover:opacity-100`}
                       >
                         <Link className="flex items-start pl-5" href={item.link}>
@@ -250,14 +265,14 @@ const pageInfoList: pageInfoItemT[] = [
               <div className="absolute top-0 left-0 z-[801] flex min-h-[100vh] w-full flex-col bg-arta-russet-100 px-[4em] pt-[6em] lg:pt-[2.8em] ">
                 <div className="flex items-start justify-between">
                   <div
-                    className="h-6 w-6 text-arta-eggshell-100 cursor-pointer"
+                    className="h-6 w-6 cursor-pointer text-arta-eggshell-100"
                     onClick={() => {
                       setShowMenu(!showMenu)
                     }}
                   >
                     <XIcon className="h-6 w-6" />
                   </div>
-                  <div className="relative cursor-pointer opacity-100 transition hover:opacity-100 md:w-auto md:h-auto w-[78.67px] h-[32px]">
+                  <div className="relative h-[32px] w-[78.67px] cursor-pointer opacity-100 transition hover:opacity-100 md:h-auto md:w-auto">
                     <img src="/images/Group.png" alt="logo" className=" object-contain" />
                   </div>
                 </div>
@@ -266,7 +281,12 @@ const pageInfoList: pageInfoItemT[] = [
                     <div className="flex flex-col space-y-8">
                       {pageInfoList.map((item, index) => (
                         <div key={index}>
-                          <MobileNavbar item={item} expand={activeMobileNavItem === index} index={index} setActiveMobileNavItem={setActiveMobileNavItem} />
+                          <MobileNavbar
+                            item={item}
+                            expand={activeMobileNavItem === index}
+                            index={index}
+                            setActiveMobileNavItem={setActiveMobileNavItem}
+                          />
                         </div>
                       ))}
                     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import type { GetServerSideProps, NextPage } from "next";
-import { useTranslation } from "next-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import type { GetServerSideProps, NextPage } from 'next'
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import React from 'react'
 import Header from 'src/components/Header/Header'
 import Footer from 'src/components/Footer'
@@ -9,13 +9,13 @@ import { Slides } from 'src/components/PageHome/PageHome'
 import { Seo } from 'src/components/Seo'
 
 const Home: NextPage = () => {
-  const { t, i18n } = useTranslation("common");
-  console.log(t)
-  console.log(i18n)
+  const { t, i18n } = useTranslation('common')
+  // console.log(t)
+  // console.log(i18n)
   return (
     <>
       <Seo title={t('site_title', { ns: 'common' })} />
-      <Header fontSize={'1em'}/>
+      <Header fontSize={'1em'} />
       <main>
         <Slides />
       </main>
@@ -26,10 +26,8 @@ const Home: NextPage = () => {
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
   props: {
-    ...(await serverSideTranslations(locale || "en", [
-      "common",
-    ])),
+    ...(await serverSideTranslations(locale || 'en', ['common'])),
   },
-});
+})
 
 export default Home

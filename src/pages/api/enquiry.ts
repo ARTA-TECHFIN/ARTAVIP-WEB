8e77cd5b3ffe9f6afdbdd402b5c3299b858f5738
CM Ngan <plusonecm@gmail.com>
2022-11-18 23:51:36 +0800

    add email

--- src/pages/api/enquiry.ts
+++ src/pages/api/enquiry.ts
@@ -1,9 +1,46 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import nodemailer from 'nodemailer'
+
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: 587,
+  secure: false,
+  logger: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+})
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log('[Enquiry Form submitted]. req.body', req.body)
-  await delay(3000)
-  res.status(200).json({})
+  try {
+    console.log('[Enquiry Form submitted]. req.body: ', req.body || 'No body')
+
+    if (process.env.EMAIL_HOST) {
+      const message = {
+        // 554 error if from is not set
+        // https://www.courier.com/error-solutions/amazon-ses-554-message-rejected-email-address-is-not-verified/
+        // need to confirm email address in AWS SES
+        // sender address must be verified with Amazon SES.
+        // recipient address must be verified with Amazon SES if sandbox mode is enabled.
+        from: 'test@twopies.io',
+        to: 'test@twopies.io',
+        subject: 'Message title',
+        text: 'Plaintext version of the message',
+        html: '<p>HTML version of the message</p>',
+      }
+
+      await transporter.sendMail(message)
+    } else {
+      await delay(1000)
+    }
+
+    res.status(200).json({})
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Internal server error' })
+  }
 }

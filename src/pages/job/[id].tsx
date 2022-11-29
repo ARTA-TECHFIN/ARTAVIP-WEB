68e82a47993be5c04735722ad70d45dd4e3ebf3a
Jason Ma <horsehcj@gmail.com>
2022-11-30 05:42:28 +0800

    Job opening details page

--- src/pages/job/[id].tsx
+++ src/pages/job/[id].tsx
@@ -1,3 +1,15 @@
+import type { GetServerSideProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
 import PageJobOpenings from 'src/components/PageJobOpenings/PageJobOpenings'
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { locale } = context
+  return {
+    props: {
+      ...(await serverSideTranslations(locale || 'en', ['common'])),
+    }
+  }
+}
 
 export default PageJobOpenings

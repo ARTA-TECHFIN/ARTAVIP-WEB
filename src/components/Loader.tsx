ca800962ac40d0632e6a1e3f64a5efad2b5d2820
Jason Ma <horsehcj@gmail.com>
2022-12-08 21:24:51 +0800

    #136 [investor Relations] Loading in mobile view

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -5,7 +5,7 @@ type propsT = {
 const Loader = ({ isLoading = true }: propsT) => {
   if (!isLoading) return null
 
-  return <div>Loading</div>
+  return <div className="my-16"><img className="w-6 h-6" src="/images/loading.svg" /></div>
 }
 
 export { Loader }

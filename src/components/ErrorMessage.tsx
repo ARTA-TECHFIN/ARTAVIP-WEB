f128258813c89cb340c2fdd99ed542278f05d6f8
Jason Ma <horsehcj@gmail.com>
2022-12-15 02:13:59 +0800

    Error message font size

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -5,9 +5,9 @@ type propsT = {
 const ErrorMessage = ({ error }: propsT) => {
   if (!error) return null
 
-  if (error instanceof Error) return <div>{error.message}</div>
+  if (error instanceof Error) return <div className="my-8 text-[1rem]">{error.message}</div>
 
-  if (typeof error === 'string') return <div>{error}</div>
+  if (typeof error === 'string') return <div className="my-8 text-[1rem]">{error}</div>
 
   console.error('Unknown error type', error)
   return <div>Unknown error</div>

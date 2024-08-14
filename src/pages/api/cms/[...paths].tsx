42d035b2659c20a2999a8bdba48017c1a30692cc
guang.li@artatechfin.com <guang.li@artatechfin.com>
2024-08-14 14:09:11 +0800

    update api system

--- src/pages/api/cms/[...paths].tsx
+++ src/pages/api/cms/[...paths].tsx
@@ -8,29 +8,37 @@ export default async function handler(
 
   let myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
+  // 设置Access-Control-Allow-Credentials,跨域请求带授权
+  myHeaders.set('Access-Control-Allow-Credentials', 'true');
+  myHeaders.set('Access-Control-Allow-Origin', '*');
+  myHeaders.append('mode', 'cors');
 
-   const { paths, ...query } = req.query;
-   let queryStr;
-   if(req.url && req.url.indexOf("?") != -1) { 
-     queryStr = req.url.split("?")[1];
-   }
-   if (!paths || !(paths instanceof Array))
-     return res.status(400).json({ message: "Invalid path" });
+  const { paths, ...query } = req.query;
+  let queryStr;
+  if (req.url && req.url.indexOf("?") != -1) {
+    queryStr = req.url.split("?")[1];
+  }
+  if (!paths || !(paths instanceof Array))
+    return res.status(400).json({ message: "Invalid path" });
 
-   const requestOptions = {
+  const requestOptions = {
     method: "GET",
-    headers: myHeaders,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "'GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization",
+    },
   };
 
-  let url ;
-  if(queryStr){
-    url= `${process.env.GM_CMS_API_ENDPOINT}/` + paths.join("/") +"?"+ queryStr;
-  }else{
-    url=  `${process.env.GM_CMS_API_ENDPOINT}/` + paths.join("/");
+  let url;
+  if (queryStr) {
+    url = `${process.env.GM_CMS_API_ENDPOINT}/` + paths.join("/") + "?" + queryStr;
+  } else {
+    url = `${process.env.GM_CMS_API_ENDPOINT}/` + paths.join("/");
   }
 
   try {
-    console.log("url:"+url)
     const response = await fetch(url, requestOptions);
     const result = await response.json();
     return res.status(200).json(result);

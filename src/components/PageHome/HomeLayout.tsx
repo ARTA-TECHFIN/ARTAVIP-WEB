20445d36174c8bb3ed1cb900c13291dd9a4be9fc
guang.li@artatechfin.com <guang.li@artatechfin.com>
2024-10-23 16:17:47 +0800

    add ppm files

--- src/components/PageHome/HomeLayout.tsx
+++ src/components/PageHome/HomeLayout.tsx
@@ -90,9 +90,8 @@ const HomeLayout: FC<propsT> = ({
           <div className="arta-container mx-auto homes">
               <div className='text-arta-sand-100 pt-2 top-text'>{homeData.t0}</div>
               <div className='small-text pt-4 text-arta-sand-100'>
+                <li><button onClick={() => {router.push(`${homeData.t5_link}`)}}>{homeData.t5}</button></li>
                 <li><button onClick={() => {router.push(`${homeData.t1_link}`)}}>{homeData.t1}</button></li>
-              </div>
-              <div className='small-text pt-4 text-arta-sand-100'>
                 <li><button onClick={() => {router.push(`${homeData.t4_link}`)}}>{homeData.t4}</button></li>
               </div>
               <div className='text-arta-sand-100 pt-2 top-text'>{homeData.t3}</div>
